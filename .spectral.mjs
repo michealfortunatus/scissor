78314f564fbf1739ec9d8fc309bf483523840262
Adeyinka J. Oresanya <adeyinkaoresanya@gmail.com>
2023-07-02 20:46:12 +0100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwODg1Nw/spectral.js?branch_slug=main&token=97724053-c4ed-498e-a279-b4bb2b229554";
+export default { extends: ruleset };
