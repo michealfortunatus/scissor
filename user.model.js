db510e64de9967b209bb4984f12900631c6a483f
Adeyinka J. Oresanya <adeyinkaoresanya@gmail.com>
2023-07-07 10:29:54 +0100

    Modified 2 files

---
+++ user.model.js
@@ -0,0 +1,32 @@
+import { model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  // generate a salt
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    // hash the password using the new salt
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+const userModel = model("User", userSchema);
+
+export { userModel };
